f3d3ac578ddb8f27d9b0e8291c7e379ff68c8398
msouldze <msouldze1@gmail.com>
2023-12-02 15:16:14 +0900

    fix margins

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,5 +1,9 @@
 import { toggle, swiper } from "./utilities.js";
 
+
+const swiperElement = document.querySelector(".swiper");
+const wrapperElement = document.querySelector(".swiper-wrapper");
+
 swiper.init();
 
 document.addEventListener("click", (e) => {
